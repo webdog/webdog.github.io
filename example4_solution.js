765651d7a77a3b3191e2ad61cc461ea4cf73c04c
Christian Weber <webdog@github.com>
2016-02-29 21:49:26 -0600

    syntax

--- example4_solution.js
+++ example4_solution.js
@@ -41,20 +41,23 @@ var gitgraph4 = new GitGraph({
         var dev_a = gitgraph4.branch({
                 parentBranch: master4,
                 name: "dev_a",
-                column: 4
+                column: 4,
+	    	showLabel: true
                 }); 
 
         var dev_b = gitgraph4.branch({
                 parentBranch: master4,
                 name: "dev_b",
-                column: 3
+                column: 3,
+	    	showLabel: true
                 }); 
         
 	
 	var dev_c = gitgraph4.branch({
 		parentBranch: master4,
 	    	name: "dev_c",
-	    	column: 2
+	    	column: 2,
+	    	showLabel: true
 		});
 
 	master4.checkout();
