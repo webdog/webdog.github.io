17737d25be68f27d54e95654c54348541ea8229b
Christian Weber <webdog@github.com>
2016-02-29 21:46:22 -0600

    syntax errors

--- example4_solution.js
+++ example4_solution.js
@@ -28,7 +28,7 @@ var myTemplateConfig = {
 var myTemplate = new GitGraph.Template( myTemplateConfig);
 var gitgraph4 = new GitGraph({
         elementId: "scenario-4",
-        template: "blackarrow",,
+        template: "blackarrow",
         orientation: "vertical",
         mode: "compact"
         }); 
