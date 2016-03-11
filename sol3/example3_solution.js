e38e3c7ba0f1ee472c228736bd04bec2593c969c
Christian Weber <webdog@github.com>
2016-03-11 15:19:35 -0600

    added resolution steps

--- sol3/example3_solution.js
+++ sol3/example3_solution.js
@@ -63,4 +63,6 @@ var gitgraph3 = new GitGraph({
 	dev_b.merge(master3).checkout();
 	master3.checkout();
 	dev_a.commit("Developer A makes another change, run git fetch to update the branch after Dev B's merger to PROD");
-
+	master3.checkout();
+	dev_a.merge(master3);
+	dev_a.commit();
