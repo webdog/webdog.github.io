d311c0b8b1433df5b13894c307541589f989d285
Christian Weber <webdog@github.com>
2016-03-11 15:29:05 -0600

    commit

--- sol3/example3_solution.js
+++ sol3/example3_solution.js
@@ -61,6 +61,7 @@ var gitgraph3 = new GitGraph({
 	master3.commit("Developer B opens a branch at this point");
         dev_b.commit("Developer B makes a commit against his branch").commit("And another").commit("This one is merged");
 	dev_b.merge(master3);
+	master3.commit();
 	dev_a.checkout(master3);
 	dev_a.commit();
 	dev_a.merge(master3);
